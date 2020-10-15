4961b1865f448337ec28e6859001c00da25b4951
bisma akram <bisma.akram97@hotmail.com>
2020-10-16 00:00:12 +0500

    update

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const app= express();
 const env= require('dotenv');
 const homeRoutes= require('./routes/home');
 const chatBotRoutes= require('./routes/chatbot');
-
+const bodyParser= require('body-parser');
 env.config();
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is listening on port ${process.env.PORT}`);
 });
+app.use(bodyParser.urlencoded({"extended": false}));
+app.use(bodyParser.json());
 app.use(express.static('./src/public'))
 app.set("view engine", "ejs")
 app.set("views", "./src/views")
