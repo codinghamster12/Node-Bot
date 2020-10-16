483387e71b756d8bb5a02529851ab9a52b0a9989
bisma akram <bisma.akram97@hotmail.com>
2020-10-16 15:31:41 +0500

    update

--- src/controllers/chatbot.js
+++ src/controllers/chatbot.js
@@ -62,55 +62,17 @@ exports.postWebHook = (req, res) => {
 };
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-  let response;
-
-  const greeting = firstTrait(message.nlp, 'wit$greetings');
-  if (greeting && greeting.confidence > 0.8) {
-    sendResponse('Hi there!');
-  } else { 
-    // default logic
-  
   
   // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              }
-            ],
-          }]
-        }
-      }
-    }
-  } 
-}
+  const greeting = firstTrait(message.nlp, 'wit$greetings');
+  if (greeting && greeting.confidence > 0.8) {
+    callSendAPI(sender_psid, { response: 'Hi there'});
+  } else { 
+    // default logic
+    callSendAPI(sender_psid, {response: 'default'});
+  }
   
   // Send the response message
-  callSendAPI(sender_psid, response);  
 }
 
 // Handles messaging_postbacks events
