4d735ae8080e7a66ccbfe1611c9c72d77829e983
bisma akram <bisma.akram97@hotmail.com>
2020-10-23 19:18:26 +0500

    'updates'

--- src/controllers/chatbot.js
+++ src/controllers/chatbot.js
@@ -101,7 +101,7 @@ function handleMessage(sender_psid, received_message) {
 
 function getFacebookUserName (sender_psid){
   return new Promise((resolve, reject) => {
-      let uri= 'https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=4${process.env.FB_PAGE_TOKEN}'
+      let uri= `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=4${process.env.FB_PAGE_TOKEN}`
   request({
     "uri": uri,
     "method": "GET",
