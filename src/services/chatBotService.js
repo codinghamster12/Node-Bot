1f2b740d9f4a19a0dc8bbc8901cf014ab7304a5b
bisma akram <bisma.akram97@hotmail.com>
2020-10-23 19:19:19 +0500

    'updates'

--- src/services/chatBotService.js
+++ src/services/chatBotService.js
@@ -1,24 +1,6 @@
 const request = require('request');
 
-let getFacebookUserName= (sender_psid)=>{
-    return new Promise((resolve, reject) => {
-        let uri= 'https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=4${process.env.FB_PAGE_TOKEN}'
-    request({
-      "uri": uri,
-      "method": "GET",
-    }, (err, res, body) => {
-        try{
-            body= JSON.parse(body);
-            let username= `${body.last_name} ${body.first_name}`;
-            resolve(username);
-        }
-        catch(err){
-            reject('Unable to send message: ' + err);
-        }
-      
-    }); 
-    });
-}
+
     
   
   
