a317261184cfde2a64129760438f805ef2bdb807
bisma akram <bisma.akram97@hotmail.com>
2020-10-23 19:12:32 +0500

    'updates'

--- src/controllers/chatbot.js
+++ src/controllers/chatbot.js
@@ -99,6 +99,26 @@ function handleMessage(sender_psid, received_message) {
   // Send the response message
 }
 
+function getFacebookUserName (sender_psid){
+  return new Promise((resolve, reject) => {
+      let uri= 'https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=4${process.env.FB_PAGE_TOKEN}'
+  request({
+    "uri": uri,
+    "method": "GET",
+  }, (err, res, body) => {
+      try{
+          body= JSON.parse(body);
+          let username= `${body.last_name} ${body.first_name}`;
+          resolve(username);
+      }
+      catch(err){
+          reject('Unable to send message: ' + err);
+      }
+    
+  }); 
+  });
+}
+
 
 // Handles messaging_postbacks events
 let handlePostback= async (sender_psid, received_postback) => {
